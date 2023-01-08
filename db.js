6b5b7dc7c37691ea7dc80d21d39cb6df93e499af
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-01-08 08:48:10 +0100

    Fix bug with connets db

--- db.js
+++ db.js
@@ -1,28 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectToDB = () => {
   // connect to DB
-  const NODE_ENV = process.env.NODE_ENV;
-  let dbUri = '';
+  const NODE_ENV = process.env.NODE_ENV
+  let dbUri = ''
 
-  if (NODE_ENV === 'production') dbUri = process.env.DB_URL;
+  if (NODE_ENV === 'production') dbUri = process.env.DB_URL
   else if (NODE_ENV === 'test')
-    dbUri = 'mongodb://localhost:27017/NoticeBoardtest';
-  else dbUri = 'mongodb://localhost:27017/NoticeBoard';
+    dbUri = 'mongodb://localhost:27017/NoticeBoardtest'
+  else dbUri = 'mongodb://localhost:27017/NoticeBoard'
 
-  mongoose.connect(dbUri, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  });
-  const db = mongoose.connection;
+  mongoose.connect(
+    'mongodb+srv://patryk_slowinski:' +
+      process.env.password +
+      '@cluster0.iqjsj6s.mongodb.net/NoticeBoard',
+  )
+  const db = mongoose.connection
 
   // on success
   db.once('open', () => {
-    console.log('Connected to the database');
-  });
+    console.log('Connected to the database')
+  })
 
   // on error
-  db.on('error', (err) => console.log('Error ' + err));
-};
+  db.on('error', (err) => console.log('Error ' + err))
+}
 
-module.exports = connectToDB;
+module.exports = connectToDB
